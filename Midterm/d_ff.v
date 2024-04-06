5f549c7c636f52fd6ed735479f8d07693c27453f
ngxx-fus <msnp@outlook.com.vn>
2024-04-06 11:50:56 +0700

    thêm clear và reset

--- Midterm/d_ff.v
+++ Midterm/d_ff.v
@@ -4,16 +4,19 @@ clear : k đồng bộ, đặt ngõ ra mức 0
 preset : k đồng bộ, đặt ngõ ra mức 1
 */
 
-module d_ff(output reg q, input d, input Clock);
+module d_ff(output reg q, input d, input clock, input clear, input preset);
     //posedge for "low-to-high edge"
     //negedge for "high-to-low edge"
     initial begin // vô kiểm tra bỏ qua cái này nhe :v
         q <= 0;
     end
-    always @(posedge Clock)
+    always @(posedge clock, clear, preset)
         begin
                 q <= d;
-                if ()
+                if (clear == 1 && preset == 1) q=q;
+                else if (clear == 1 ) q = 0 ;
+                else if (preset == 1) q=1;
+                else q=d;
         end
 endmodule
 
