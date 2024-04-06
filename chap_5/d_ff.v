d09604d9c812f9a18aa5330a48e60d497e37d58a
ngxx-fus <msnp@outlook.com.vn>
2024-04-06 11:10:04 +0700

    kajfd

--- chap_5/d_ff.v
+++ chap_5/d_ff.v
@@ -2,7 +2,7 @@
 Module name: D flipflop
 Author: Nguyen Thanh Phu
 */
-
+//low
 module d_ff(output reg q, input d, input Clock);
     //posedge for "low-to-high edge"
     //negedge for "high-to-low edge"
