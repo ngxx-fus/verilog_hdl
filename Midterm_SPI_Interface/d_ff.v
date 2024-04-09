9775d19b0e2ed40d359710194a4b7eb06e75b967
fus <msnp@outlook.com.vn>
2024-04-10 01:05:00 +0700

    v.1.3 - Cập nhật độ ưu tiên

--- Midterm_SPI_Interface/d_ff.v
+++ Midterm_SPI_Interface/d_ff.v
@@ -2,6 +2,7 @@
 Module name: D flipflop
 CLR : k đồng bộ, đặt ngõ ra mức 0 khi CLR = 1, còn lại 0,z thì k tác động
 PRE : k đồng bộ, đặt ngõ ra mức 1 khi PRE = 1, còn lại 0,z thì k tác động
+NOTE: Khi cả posedge CLK, CLR, PRE cùng tác động thì ưu tiên CLR, PRE
 */
 
 module d_ff(output reg Q, input DATA, input CLK, input CLR, input PRE);
