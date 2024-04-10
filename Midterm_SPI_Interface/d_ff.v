9aecaa9dff53779ee2f05292a73fff66d5533607
fus <msnp@outlook.com.vn>
2024-04-11 00:20:44 +0700

    v1.7

--- Midterm_SPI_Interface/d_ff.v
+++ Midterm_SPI_Interface/d_ff.v
@@ -1,31 +1,21 @@
 `timescale 100ps/100ps
 /*
 Module name: D flipflop
-CLR : k đồng bộ, đặt ngõ ra mức 0 khi CLR = 1, còn lại 0,z thì k tác động
-PRE : k đồng bộ, đặt ngõ ra mức 1 khi PRE = 1, còn lại 0,z thì k tác động
-NOTE: Khi cả posedge CLK, CLR, PRE cùng tác động thì ưu tiên CLR, PRE
+CLR : k d?ng b?, d?t ngõ ra m?c 0 khi CLR = 1, còn l?i 0,z thì k tác d?ng
+PRE : k d?ng b?, d?t ngõ ra m?c 1 khi PRE = 1, còn l?i 0,z thì k tác d?ng
+NOTE: Khi c? posedge CLK, CLR, PRE cùng tác d?ng thì uu tiên CLR, PRE
 */
 
 module d_ff(output reg Q, input DATA, input CLK, input CLR, input PRE);
     //posedge for "low-to-high edge"
     //negedge for "high-to-low edge"
-    initial begin // vô kiểm tra bỏ qua cái này nhe :v
+    initial begin // vô ki?m tra b? qua cái này nhe :v
         Q = 1'b0;
         end
-    always @(CLR, PRE)
+    always @(CLK, CLR, PRE)
         begin
             if( CLR == 1'b0 && PRE == 1'b0)
-                Q = Q;
-            else if(CLR == 1'b1 && PRE == 1'b0)
-                #5 Q = 1'b0;
-            else if(CLR == 1'b0 && PRE == 1'b1)
-                #5 Q = 1'b1;
-            else #5 Q = Q;
-        end
-    always @(posedge CLK)
-        begin
-            if( CLR == 1'b0 && PRE == 1'b0)
-                #5 Q = DATA;
+                Q = (CLK == 1)?DATA:Q;
             else if(CLR == 1'b1 && PRE == 1'b0)
                 #5 Q = 1'b0;
             else if(CLR == 1'b0 && PRE == 1'b1)
